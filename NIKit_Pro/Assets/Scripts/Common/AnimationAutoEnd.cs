a7bb8e95ffc8ee30286c995b18983820cfd55380
WinMi <357622852@qq.com>
2018-10-17 21:56:23 +0800

    no message

---
+++ NIKit_Pro/Assets/Scripts/Common/AnimationAutoEnd.cs
@@ -0,0 +1,33 @@
+﻿/// <summary>
+/// AnimationAutoEnd.cs
+/// Created by Who 2015-01-08
+/// 动画自动结束组件
+/// </summary>
+
+using UnityEngine;
+using System.Collections;
+
+public partial class AnimationAutoEnd : MonoBehaviour
+{
+    #region 公共函数
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 如果动画播放结束，则销毁本光效
+        Animator aor = gameObject.GetComponent<Animator>();
+        if (aor == null ||
+            1.0f < aor.GetCurrentAnimatorStateInfo(CombatConfig.ANIMATION_BASE_LAYER_INEDX).normalizedTime)
+        {
+            GameObject.Destroy(gameObject);
+            return;
+        }
+    }
+
+    #endregion
+}
---
+++ NIKit_Pro/Assets/Scripts/Common/Formula.cs
@@ -0,0 +1,34 @@
+﻿/// <summary>
+/// Formula.cs
+/// Copy from zhangyg 2014-10-22
+/// 公式基类
+/// </summary>
+
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+// 脚本管理
+public abstract class Formula
+{
+    #region 公共接口
+
+    /// <summary>
+    /// 调用公式
+    /// </summary>
+    public object Invoke(params object[] args)
+    {
+        // 获取公式入口函数
+        Type type = this.GetType();
+        MethodInfo methodInfo = type.GetMethod("Call");
+
+        // 没有该接口不处理
+        if(methodInfo == null)
+            return null;
+
+        // 调用公式计算
+        return methodInfo.Invoke(this, args);
+    }
+
+    #endregion
+}
