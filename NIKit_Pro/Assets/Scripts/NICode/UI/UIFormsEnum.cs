3ff0b7d7b639cf73be3910eea17ce4173c69e86c
WinMi <357622852@qq.com>
2018-10-31 22:09:22 +0800

    no message

--- NIKit_Pro/Assets/Scripts/NICode/UI/UIFormsEnum.cs
+++ NIKit_Pro/Assets/Scripts/NICode/UI/UIFormsEnum.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,13 +6,15 @@ using UnityEngine;
 public enum UIFormsType
 {
     Normal = 1, //基础功能界面窗口
+
     Popup = 2,  //弹窗类型
 }
 
 public enum UIFormsLifeType
-{ 
+{
     //频繁打开的窗口(背包，英雄界面)此种窗口需要缓存,关闭此种窗口，就是隐藏，没有销毁
     GoldLife = 1,
+
     //无需缓存的窗口,关闭此种窗口，就是销毁
     HumanLife,
 }
@@ -20,11 +22,18 @@ public enum UIFormsLifeType
 public enum UIFormsLayer
 {
     MainUILayer = 1, //主界面层
+
     CommonUILayer = 1000, //通用层(1.功能层 2.弹窗层)
+
     RewardLayer = 2000, //物品奖励层
+
     TipLayer = 3000, //Tip层
+
     NotifyLayer = 4000, //跑马灯层(全服公告内容等等)
+
     GuideLayer = 5000, //引导层
+
     LoadingLayer = 7000, //Loading加载层
-    ServerLayer = 10000, //网络转菊花层     
+
+    ServerLayer = 10000, //网络转菊花层
 }
